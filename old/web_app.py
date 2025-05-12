dafa106db956ce3a546d5247fc3e2ce71a57ce79
buyun14 <2135481470@qq.com>
2025-05-12 17:01:41 +0800

    refactor(old): 修改 Web 服务器端口
    - 将 Web 服务器的端口从 5000 更改为 5004

--- old/web_app.py
+++ old/web_app.py
@@ -169,4 +169,4 @@ if __name__ == '__main__':
     simulator_thread.start()
     
     # 启动Web服务器
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=5004) 
